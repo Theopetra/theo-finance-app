a0f85de4612a47385aae67de03e2a23ddc926591
redgorilla <redgorilla@theopetralabs.com>
2022-03-13 19:52:07 -0500

    use class string

--- components/Navigation/NavBar.tsx/index.tsx
+++ components/Navigation/NavBar.tsx/index.tsx
@@ -13,7 +13,7 @@ const NavBar = ({ sidebarOpen, setSidebarOpen }: NavDrawerProps) => {
     <div className="fixed bottom-0 z-50 flex justify-between w-full h-16 pt-1 pl-1 bg-white dark:bg-black sm:pl-3 sm:pt-3 lg:hidden">
       <div className={classes.statContainer}>
         <div className={classes.number}>101,221,000</div>
-        <div className="text-xs font-normal text-theo-cyan">Your $THEO</div>
+        <div className={classes.label}>Your $THEO</div>
       </div>
       <div className="flex items-center justify-center flex-1 mx-auto -mt-16">
         <button type="button" onClick={() => setSidebarOpen(!sidebarOpen)}>
