b8e6c6054e1923297adba57cdd3548a06136366a
redgorilla <redgorilla@theopetralabs.com>
2022-04-27 06:25:00 -0500

    update page name

--- pages/nav-config.tsx
+++ pages/nav-config.tsx
@@ -2,9 +2,10 @@ export const navigation = [
   { name: 'Dashboard', href: '/', icon: 'gauge' },
   { name: 'Discount Buy', href: '/discount-buy', icon: 'intersect' },
   {
-    name: 'Stake + Grow',
-    href: '/stake-and-grow',
+    name: 'Memberships',
+    href: '/memberships',
     icon: 'lock-laminated',
+    disabled: true,
   },
   { name: 'T-Homes', href: '/t-homes', disabled: true, icon: 'door' },
   { name: 'Learn More', href: '/learn-more', icon: 'file' },
