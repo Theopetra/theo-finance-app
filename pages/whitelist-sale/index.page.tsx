e9b7fe14101adba3509984dd17eb501e1fe1c289
redgorilla <redgorilla@theopetralabs.com>
2022-04-27 21:37:13 -0500

    add shadow

--- pages/whitelist-sale/index.page.tsx
+++ pages/whitelist-sale/index.page.tsx
@@ -39,6 +39,7 @@ const ACTION_CARD = [
     ethPrice: '.00025',
     usdcPrice: '1.00',
     warning: 'Important: New buys are auto-locked until completion of the term',
+    highlight: true,
   },
 ];
 
@@ -86,29 +87,30 @@ const WhitelistSale = () => {
         ))}
       </CardList>
       <CardList>
-        {ACTION_CARD.map(({ header, actionButton, warning, ethPrice, usdcPrice }, i) => (
+        {ACTION_CARD.map(({ header, actionButton, warning, ethPrice, usdcPrice, highlight }, i) => (
           <Fragment key={`${header}_${i}`}>
             <Card
+              className={`${highlight ? 'drop-shadow-[6px_6px_6px_rgba(80,174,203,0.75)]' : ''}`}
               darkModeBgColor="bg-black dark:bg-none"
-              headerClasses="bg-theo-navy text-white"
+              headerClasses="bg-theo-navy text-white dark:bg-theo-dark-navy"
               title={
                 <div className="flex items-center pr-2">
                   <div className="font- mr-2 text-2xl font-extrabold">{header}</div>
                 </div>
               }
-              headerRightComponent={<Icon name="lock-laminated" className="w-10" />}
+              headerRightComponent={<Icon name="lock-laminated" className="w-8" />}
             >
               <div className="flex flex-1 flex-col justify-between">
                 <div className="mb-8 flex-1 space-y-4">
-                  <div className="flex justify-between text-lg text-theo-navy">
+                  <div className="flex justify-between text-lg text-theo-navy dark:text-theo-cyan">
                     <div>Asset</div>
                     <div>Price / THEO</div>
                   </div>
-                  <div className="flex justify-between rounded-lg bg-[#e3e3e3] p-5">
+                  <div className="flex justify-between rounded-lg bg-[#e3e3e3] p-5 dark:bg-[#262626]">
                     <div>Logo</div>
                     <div className="text-xl">{usdcPrice}</div>
                   </div>
-                  <div className="flex justify-between rounded-lg bg-[#e3e3e3] p-5">
+                  <div className="flex justify-between rounded-lg bg-[#e3e3e3] p-5 dark:bg-[#262626]">
                     <div>Logo</div>
                     <div className="text-xl">{ethPrice}</div>
                   </div>
