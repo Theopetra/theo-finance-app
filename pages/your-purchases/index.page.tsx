097020db2174b67347f02a944b82faa6026f4cef
redgorilla <redgorilla@theopetralabs.com>
2022-05-03 07:13:13 -0500

    create page

---
+++ pages/your-purchases/index.page.tsx
@@ -0,0 +1,11 @@
+import PageContainer from "@/components/PageContainer";
+
+const YourPurchases = () => {
+  return <PageContainer>your purchases Content</PageContainer>;
+};
+
+YourPurchases.PageHead = () => {
+  return 'Your Purchases';
+};
+
+export default YourPurchases;
