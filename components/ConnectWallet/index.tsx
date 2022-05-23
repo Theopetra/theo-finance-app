ef0146443abaa478213d5b28335d964ca2b81854
redgorilla <redgorilla@theopetralabs.com>
2022-05-23 07:49:40 -0500

    more minimal wallet ui

--- components/ConnectWallet/index.tsx
+++ components/ConnectWallet/index.tsx
@@ -1,18 +1,7 @@
-import { defaultConfig } from 'next/dist/server/config-shared';
-import Icon from '../Icons';
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 
 const ConnectWallet = () => {
-  const handleClick = () => {
-    window.alert('Connect Wallet');
-  };
-  return (
-    <ConnectButton />
-    // <button onClick={handleClick} className="border-button space-x-2">
-    //   <Icon name="wallet" className="w-4" />
-    //   <span> connect wallet</span>
-    // </button>
-  );
+  return <ConnectButton accountStatus="address" chainStatus="icon" showBalance={false} />;
 };
 
 export default ConnectWallet;
