ba9fbbca3ada34f698993e284e1693edb7310778
redgorilla <redgorilla@theopetralabs.com>
2022-05-23 06:40:13 -0500

    successfull connection

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -5,6 +5,8 @@ import PageContainer from '@/components/PageContainer';
 import StatCard from '@/components/StatCard';
 import { useTheme } from '@/state/ui/theme';
 import { Fragment } from 'react';
+import { useContractRead } from 'wagmi';
+import TheopetraBondDeposiotory from './TheopetraBondDepository.json';
 
 const STATS = [
   {
@@ -35,9 +37,18 @@ const PROPERTY_MANAGEMENT = [
     lightImgSrc: '/assets/images/dashboard/Graph_light_version.png',
   },
 ];
+
 const Dashboard = () => {
   const [{ theme }] = useTheme();
-
+  const { data, isError, isLoading } = useContractRead(
+    {
+      addressOrName: '0x7130212e81e74db3BA13cE052B93a7E5F1Df00B3',
+      contractInterface: TheopetraBondDeposiotory.abi,yarb
+    },
+    'liveMarkets'
+  );
+  console.log(data);
+  
   return (
     <>
       <div className="pt-4">
