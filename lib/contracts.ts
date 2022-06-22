03b4cf67270936989636d72f4f08779512e2dbbf
BowTiedAztec <bowtiedaztec@tutanota.com>
2022-06-22 15:38:42 -0500

    Deprecating rinkeby

--- lib/contracts.ts
+++ lib/contracts.ts
@@ -7,7 +7,7 @@ import whitelistBondDepArtifact from '../artifacts/WhitelistTheopetraBondDeposit
 // TODO: change once this is deployed
 import PublicPreListBondDepositoryArtifact from '../artifacts/WhitelistTheopetraBondDepository.json';
 
-// address 1 = mainnet, address 3 = ropsten, address 4 = rinkeby
+// address 1 = mainnet, address 3 = ropsten
 // TODO: update mainnet addresses for launch
 
 export const contractMetadata = {
@@ -16,7 +16,6 @@ export const contractMetadata = {
     address: {
       1: '0x4805Ea3b3D3f6eDf4f632FD02e53c2aD53422356',
       3: '0x4805Ea3b3D3f6eDf4f632FD02e53c2aD53422356',
-      4: '0x2E48f1E6C53ace80BA34F4f138d9b4A7488ca9E9',
     },
     abi: PublicPreListBondDepositoryArtifact.abi,
   },
@@ -24,7 +23,6 @@ export const contractMetadata = {
     address: {
       1: '0xb00b8fD9C8543e546064Fb087363E98B619D23b1',
       3: '0xb00b8fD9C8543e546064Fb087363E98B619D23b1',
-      4: '0x0ee54Aa3fE9695Eff297582080Bd9766D09FBD9A',
     },
     abi: stakingDistributorArtifact.abi,
   },
@@ -32,7 +30,6 @@ export const contractMetadata = {
     address: {
       1: '0x7DC47D9469f527F971d7062637eDe4d2a071c7B6',
       3: '0x7DC47D9469f527F971d7062637eDe4d2a071c7B6',
-      4: '0xCD1a66F06eC36Db3F040C6065e5AAC0866FcD77A',
     },
     abi: sTheopetraArtifact.abi,
   },
@@ -40,7 +37,6 @@ export const contractMetadata = {
     address: {
       1: '0xD19E2973068657E68f4344bCB82E233864d302d7',
       3: '0xD19E2973068657E68f4344bCB82E233864d302d7',
-      4: '0xBcdF034cE6624A817c1BfEffBDE8691443e5fDbB',
     },
     abi: theopetraAuthorityArtifact.abi,
   },
@@ -48,7 +44,6 @@ export const contractMetadata = {
     address: {
       1: '0x2C33Ef90294Ca8e8fF6e3EA9dE099b6AF5b697F8',
       3: '0x2C33Ef90294Ca8e8fF6e3EA9dE099b6AF5b697F8',
-      4: '0x7130212e81e74db3BA13cE052B93a7E5F1Df00B3',
     },
     abi: theopetraBondRepositoryArtifact.abi,
   },
@@ -56,7 +51,6 @@ export const contractMetadata = {
     address: {
       1: '0x4805Ea3b3D3f6eDf4f632FD02e53c2aD53422356',
       3: '0x4805Ea3b3D3f6eDf4f632FD02e53c2aD53422356',
-      4: '0xbCF05b9993B5241C9F46F8a4C3459d423299D57D',
     },
     abi: whitelistBondDepArtifact.abi,
   },
@@ -64,7 +58,6 @@ export const contractMetadata = {
     address: {
       1: '0x4805Ea3b3D3f6eDf4f632FD02e53c2aD53422356',
       3: '0x4805Ea3b3D3f6eDf4f632FD02e53c2aD53422356',
-      4: '0x2E48f1E6C53ace80BA34F4f138d9b4A7488ca9E9',
     },
     abi: wethHelper.abi,
   },
