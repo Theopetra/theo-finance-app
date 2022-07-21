ede1bdb547428948e2536e1383f17e7584989bd2
Glasscloud <glasscloudweb@protonmail.com>
2022-07-21 11:53:38 -0400

    updated tone of copy and changed all references to

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -79,7 +79,7 @@ const Dashboard = ({ locked }) => {
     {
       name: (
         <>
-          TOTAL $THEO Locked -<br/>
+          TOTAL $THEO Locked<br/>
           <strong>6 Months</strong>
         </>
       ),
@@ -90,7 +90,7 @@ const Dashboard = ({ locked }) => {
     {
       name: (
         <>
-          TOTAL $THEO Locked -<br/>
+          TOTAL $THEO Locked<br/>
           <strong>12 Months</strong>
         </>
       ),
@@ -101,7 +101,7 @@ const Dashboard = ({ locked }) => {
     {
       name: (
         <>
-          TOTAL $THEO Locked -<br/>
+          TOTAL $THEO Locked<br/>
           <strong>18 Months</strong>
         </>
       ),
