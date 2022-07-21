33d48e151534f74386d7aa1caf30d617cc07ef0b
Glasscloud <glasscloudweb@protonmail.com>
2022-07-21 19:03:10 -0400

    removed mock data and updated verbiage

--- components/Navigation/NavBar.tsx/index.tsx
+++ components/Navigation/NavBar.tsx/index.tsx
@@ -1,9 +1,13 @@
 import homeButtonLight from '../assets/homeButtonlight.png';
 import homeButtonDark from '../assets/homeButtonDark.png';
+import { useUserPurchases } from '@/pages/discount-buy/state/use-user-purchases';
+import { formatTheo } from '@/lib/format_theo';
+import { BigNumber } from 'ethers';
 import { useTheme } from '@/state/ui/theme';
 
 const NavBar = () => {
   const [{ theme, navigationOpen }, { setNavigationOpen }] = useTheme();
+  const [{ purchases }]: any = useUserPurchases();
   const classes = {
     statContainer: 'flex flex-col items-center justify-center flex-1 ',
     number: 'font-bold text-theo-cyan dark:text-white',
@@ -13,7 +17,13 @@ const NavBar = () => {
   return (
     <div className="fixed bottom-0 z-30 flex justify-between w-full h-16 pt-1 pl-1 bg-white dark:bg-black sm:pl-3 sm:pt-3 lg:hidden">
       <div className={classes.statContainer}>
-        <div className={classes.number}>101,221,000</div>
+        <div className={classes.number}>
+          {formatTheo(
+            purchases
+              .reduce((prev, curr, i) => prev.add(curr.payout_), BigNumber.from(0))
+              .toString()
+          )}
+        </div>
         <div className={classes.label}>Your $THEO</div>
       </div>
       <div className="flex items-center justify-center flex-1 mx-auto -mt-16">
