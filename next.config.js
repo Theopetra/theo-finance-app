3093499c973ed152bda080d32d293a5c48c4ed38
redgorilla <redgorilla@theopetralabs.com>
2023-05-03 07:55:30 -0500

    don't optimize images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
