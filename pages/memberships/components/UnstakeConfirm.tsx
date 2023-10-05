00599ca7e71f1147a7fcd97dcd31bf817fa9e4b3
HomicidalChicken <86430321+Cryptofowl@users.noreply.github.com>
2023-10-05 19:37:52 -0400

    Update unstaking warning

--- pages/memberships/components/UnstakeConfirm.tsx
+++ pages/memberships/components/UnstakeConfirm.tsx
@@ -107,15 +107,14 @@ const UnstakeConfirm = ({
           <div className=" mx-auto mb-4 flex max-w-2xl flex-col gap-4 text-xl">
             <p>Premium stakes are locked for 1 year from the date of staking. </p>
             <p>
-              Premature unstaking—that is, unstaking before the 1-year lock period expires—will
-              incur a slashing penalty.
+              Staking is being phased out as part of the tokenomics upgrade, so all unstaking penalties have been removed.
             </p>
             <p>
-              These penalties are in the form of $THEO principal and $THEO rebate slashing. This
-              slashing penalty is present to incentivize long-term behavior for Premium members.
+              Thank you for staking for the first two rebates! Be sure to check back soon for more updates!
             </p>
             <div className="text-center text-2xl font-bold">
-              -{penalty ? formatTheo(BigInt(penalty)) : 0} $THEO
+              -0 $THEO
+              {/* -{penalty ? formatTheo(BigInt(penalty)) : 0} $THEO */}
             </div>
           </div>
         )}
