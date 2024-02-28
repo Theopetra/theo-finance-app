33a5cf778eece71b9ddb6ca0887af07cc42ac0c3
redgorilla <redgorilla@theopetralabs.com>
2024-02-28 05:34:20 -0600

    add svg

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -4,6 +4,8 @@ import PageContainer from '@/components/PageContainer';
 import { UserPurchasesProvider } from './market-cards/state/UserPurchasesProvider';
 import BuyFormProvider from './market-cards/state/BuyFormProvider';
 import DiscountBuyCard from './market-cards/discount-buy';
+import Card from '@/components/Card';
+import Image from 'next/image';
 
 const Dashboard = ({}) => {
   return (
@@ -17,14 +19,28 @@ const Dashboard = ({}) => {
             Growth Markets allow you to buy THEO directly from the protocol & allocate your funding
             to the project of your choice. When you buy THEO, it is locked for a period of time
             before you can claim it.{' '}
-            <a href="https://docs.theopetralabs.com/on-chain-ecosystem/growth-markets" className="underline">
+            <a
+              href="https://docs.theopetralabs.com/on-chain-ecosystem/growth-markets"
+              className="underline"
+            >
               {' '}
               Learn More
             </a>
           </p>
-          <div className="mb-14 flex max-w-lg flex-col gap-x-2 space-x-0 space-y-4 sm:space-x-2  md:flex-row">
-            <div className="flex w-full grid-cols-2 space-x-4">
+          <div className=" mb-14 flex flex-col gap-x-2 space-x-0 space-y-4 sm:space-x-2  md:flex-row">
+            <div className=" flex w-full flex-col space-x-4 md:flex-row">
               <DiscountBuyCard />
+              <Card>
+                <div className="m-10">
+                  <Image
+                    src="/assets/images/dashboard/theo-steps.svg"
+                    width={330}
+                    height={417}
+                    alt="steps"
+                    className="h-auto w-full max-w-[330px]"
+                  />
+                </div>
+              </Card>
               {/* <DiscountBuyMobyCard /> */}
             </div>
           </div>
